7128635238d7586523c81248685eff95487fc797
Ian Horsman <ianphorsman@gmail.com>
2018-01-06 14:50:15 -0500

    Added blank readme, init.py, and main Scraper class

---
+++ scraper.py
@@ -0,0 +1,19 @@
+import requests
+
+
+class Scraper(object):
+
+    def __init__(self):
+        pass
+
+    def scrape(self, drug_name):
+        pass
+
+    def return_json(self):
+        pass
+
+    def return_csv(self):
+        pass
+
+
+scraper = Scraper()
