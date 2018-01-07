de8cea4a5efeee731cb4a2f6513289ad91189e05
Ian Horsman <ianphorsman@gmail.com>
2018-01-07 01:14:05 -0500

    Can now get similarly acting drugs

--- DrugHelpers.py
+++ DrugHelpers.py
@@ -1,5 +1,7 @@
 import json
 import requests
+from collections import Counter
+from functools import reduce
 import os
 import _pickle as picklerick
 from pprint import pprint as pp
@@ -16,7 +18,7 @@ class DrugHelpers(object):
             'MOA': 'Mechanism of Action',
             'PK': 'Pharmacokinetics',
             'PE': 'Physiological Effect',
-            'Chem': 'Chemical Name',
+            'CHEM': 'Chemical Name',
             'MESHPA': 'MeSH Pharmacological Actions',
             'EPC': 'Established Pharmacological Classes',
             'ATC1-4': 'Anatomical Therapeutic Chemical',
@@ -25,83 +27,126 @@ class DrugHelpers(object):
 
     def memo(func):
         def wrapper(self, drug_name):
-            if drug_name not in self.class_memo:
+            if drug_name not in self.memo:
                 self.get_class_data_of_drug(drug_name)
             return func(self, drug_name)
+        return wrapper
+
+    def deep_memo(func):
+        def wrapper(self, drug_name, drug_property):
+            if drug_name not in self.memo:
+                self.get_class_data_of_drug(drug_name)
+            if drug_property not in self.memo[drug_name]:
+                getattr(self, drug_property)(drug_name)
+            return func(self, drug_name, drug_property)
         return wrapper
 
 
     def get_class_data_of_drug(self, drug_name):
         ret = self.api.find_class_by_drug_name(drug_name)
         classes = {
-            (source['rxclassMinConceptItem']['className'], source['rxclassMinConceptItem']['classType'])
+            (source['rxclassMinConceptItem']['className'],
+             source['rxclassMinConceptItem']['classType'],
+             source['rxclassMinConceptItem']['classId'])
             for source in ret['rxclassDrugInfoList']['rxclassDrugInfo']
         }
         arranged_classes = {}
+        #pdb.set_trace()
         for tup in classes:
             if tup[1] not in arranged_classes:
-                arranged_classes[tup[1]] = [tup[0]]
+                arranged_classes[tup[1]] = [(tup[2], tup[0])]
             else:
-                arranged_classes[tup[1]].append(tup[0])
-        self.class_memo[drug_name] = arranged_classes
+                arranged_classes[tup[1]].append((tup[2], tup[0]))
+        self.memo[drug_name] = arranged_classes
         return arranged_classes
+
+    def get_similarly_acting_drugs(self, drug_name):
+        moa_id, moa = self.memo[drug_name]['MOA'][0]
+        opts = {
+            'relaSource': 'DAILYMED',
+            'rela': "has_{}".format('MOA')
+        }
+        ret = bot.api.get_class_members(moa_id, opts)
+        return (moa, [member['minConcept']['name'] for member in ret['drugMemberGroup']['drugMember']])
+
+    def get_contraindications(self, drug_name):
+        pass
+
+    def get_class_by_name(self, class_name):
+        return self.api.find_class_by_name(class_name)['rxclassMinConceptList']['rxclassMinConcept'][0]
+
 
     @memo
     def mechanism_of_action(self, drug_name):
         moa = set()
         meshpa = set()
-        if 'MOA' in self.class_memo[drug_name]:
-            moa = set(self.class_memo[drug_name]['MOA'])
-        if 'MESHPA' in self.class_memo[drug_name]:
-            meshpa = set(self.class_memo[drug_name['MESHPA']])
+        if 'MOA' in self.memo[drug_name]:
+            moa = set([tup[1] for tup in self.memo[drug_name]['MOA']])
+        if 'MESHPA' in self.memo[drug_name]:
+            meshpa = set([tup[1] for tup in self.memo[drug_name]['MESHPA']])
 
         return sorted(moa & meshpa)
 
     @memo
     def drug_type(self, drug_name):
-        if 'VAD' not in self.class_memo[drug_name]:
+        if 'VA' not in self.memo[drug_name]:
             return
-        return sorted(set(self.class_memo[drug_name]['VA']))
+        return sorted(set([tup[1] for tup in self.memo[drug_name]['VA']]))
 
     @memo
     def indications(self, drug_name):
-        if 'DISEASE' not in self.class_memo[drug_name]:
+        if 'DISEASE' not in self.memo[drug_name]:
             return
-        return sorted(set(self.class_memo[drug_name]['DISEASE']))
+        return sorted(set([tup[1] for tup in self.memo[drug_name]['DISEASE']]))
 
     @memo
     def physiological_effects(self, drug_name):
-        if 'PE' not in self.class_memo[drug_name]:
+        if 'PE' not in self.memo[drug_name]:
             return
-        return sorted(set(self.class_memo[drug_name]['PE']))
+        return sorted(set([tup[1] for tup in self.memo[drug_name]['PE']]))
 
     @memo
     def pharmacokinetics(self, drug_name):
-        if 'PK' not in self.class_memo[drug_name]:
+        if 'PK' not in self.memo[drug_name]:
             return
-        return sorted(set(self.class_memo[drug_name]['PK']))
+        return sorted(set([tup[1] for tup in self.memo[drug_name]['PK']]))
 
     @memo
-    def pharmacological_class(self, drug_name):
-        if 'EPC' not in self.class_memo[drug_name]:
+    def therapeutic_class(self, drug_name):
+        if 'ATC1-4' not in self.memo[drug_name]:
             return
-        return sorted(set(self.class_memo[drug_name]['EPC']))
+        return sorted(set([tup[1] for tup in self.memo[drug_name]['ATC1-4']]))
+
+    @memo
+    def pharmacological_classes(self, drug_name):
+        if 'EPC' not in self.memo[drug_name]:
+            return
+        return sorted(set([tup[1] for tup in self.memo[drug_name]['EPC']]))
 
 
     @memo
     def chemical_name_of_brand(self, brand_name):
-        pass
+        if 'CHEM' not in self.memo[brand_name]:
+            return
+        return Counter([tup[1] for tup in self.memo[brand_name]['CHEM']]).most_common(1)[0][0]
 
     def drug_names_in_class(self, class_name):
         pass
 
-    @memo
-    def basic_information_of_drug(self, drug_name):
-        pass
+    def drug_info(self, drug_name):
+        return {
+            'Name': self.chemical_name_of_brand(drug_name),
+            'Drug Type': self.drug_type(drug_name),
+            'Therapeutic Class': self.therapeutic_class(drug_name),
+            'Indications': self.indications(drug_name),
+            'Mechanism of Action': self.mechanism_of_action(drug_name),
+            'Physiological Effects': self.physiological_effects(drug_name),
+            'Pharmacokinetics': self.pharmacokinetics(drug_name),
+        }
 
     def save(self, filename='class_memo'):
         with open("{}.p".format(filename), 'wb') as f:
-            picklerick.dump(self.class_memo, f)
+            picklerick.dump(self.memo, f)
 
     def wipe(self, filename='class_memo'):
         open("{}.p".format(filename), 'wb').close()
@@ -118,17 +163,15 @@ class DrugHelpers(object):
         return data
 
     def __enter__(self):
-        self.class_memo = self.load()
+        self.memo = self.load()
         return self
 
     def __exit__(self, type, value, traceback):
         self.save()
 
+
 with DrugHelpers() as bot:
-    #pp(bot.indications('fluoxetine'))
-    #pp(bot.physiological_effects('fluoxetine'))
-    #pp(bot.pharmacokinetics('fluoxetine'))
-    #pp(bot.mechanism_of_action('fluoxetine'))
-    pp(bot.drug_type('fluoxetine'))
+    pp(bot.drug_info('fluoxetine'))
     #pp(bot.get_class_data_of_drug('fluoxetine'))
-    #pp(bot.indications('fluoxetine'))
+    pp(bot.get_similarly_acting_drugs('fluoxetine'))
+
--- test.py
+++ test.py
@@ -1,11 +1,17 @@
-from RxAPIWrapper import RxDrugAPIWrapper
+from RxAPIWrapper import RxAPIWrapper
+from DrugHelpers import DrugHelpers
+from pprint import pprint as pp
+
 
 class APITester(object):
 
     def __init__(self):
-        pass
+        self.api = RxAPIWrapper()
 
     def test_api_calls(self):
         pass
 
-tests = APITester()
+    def test_drug_helpers(self):
+        with DrugHelpers() as helper:
+            helper.drug_info('fluoxetine')
+
